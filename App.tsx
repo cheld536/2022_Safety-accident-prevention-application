fb43013f48c8446156bf607e63d641ed139a1c9f
cocosasa <wjdgns0631@gmail.com>
2022-08-01 14:38:21 +0900

    Update App.tsx
    Test : Change "Step One" to "Step Two"

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step Two">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
